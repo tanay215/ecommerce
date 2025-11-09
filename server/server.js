90d507aa6ec51bbf8c7afd65e68593baee7742bc
tanay215 <tanaytenginkai@gmail.com>
2025-11-09 23:45:10 +0530

    change of url

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const PORT=process.env.PORT || 5000;
 
 app.use(
     cors({
-        origin : ['http://localhost:5173','https://ecommerce-indol-ten.vercel.app/'],
+        origin : ['http://localhost:5173','https://ecommerce-tau-three-44.vercel.app'],
         methods : ['GET','POST','DELETE','PUT'],
         allowedHeaders:[
             'Content-Type',
